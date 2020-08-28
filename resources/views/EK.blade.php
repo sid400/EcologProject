eb270d32e02ae2399c491cbb4c8f91efe629dfe0
Pavel Ivanov <p200300@mail.ru>
2020-08-28 18:41:45 +0300

    delete any hiiden rows from header

--- resources/views/EK.blade.php
+++ resources/views/EK.blade.php
@@ -1,29 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    {{--TODO API для обработки запроса от заказа--}}
-    {{--TODO Почтовая рассылка--}}
-    {{--TODO Рахобраться с фреворком для отображения пригыющих цифр(сотри закладки)--}}
-    {{--TODO Блок успешного заказа, обнуление корзины--}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <!-- Scripts -->
-    {{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.4/gsap.min.js"></script>--}}
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/Ekolog.js') }}" defer></script>
-    <!-- Fonts -->
-{{--    <link rel="dns-prefetch" href="//fonts.gstatic.com">--}}
-{{--    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">--}}
-
-<!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/EK.css') }}" rel="stylesheet">
-
-    <!-- Favico -->
     <link href="{{ asset('SVG/icons/Favicon.svg') }}" rel="shortcut icon" type="image/x-icon">
     <title>Ekolog</title>
 </head>
